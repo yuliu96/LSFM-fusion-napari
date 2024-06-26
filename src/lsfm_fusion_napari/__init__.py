a1eb576916b509fd2fd632aacc256f25923968aa
lennartkowitz <lennart.kowitz@isas.de>
2024-06-26 14:14:52 +0200

    initial setup

--- src/lsfm_fusion_napari/__init__.py
+++ src/lsfm_fusion_napari/__init__.py
@@ -1,12 +1,8 @@
 __version__ = "0.0.1"
-from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
-from ._writer import write_multiple, write_single_image
+from ._widget import FusionWidget
+from ._writer import write_tiff
 
 __all__ = (
-    "write_single_image",
-    "write_multiple",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
+    "write_tiff",
+    "FusionWidget",
 )
--- src/lsfm_fusion_napari/_writer.py
+++ src/lsfm_fusion_napari/_writer.py
@@ -8,57 +8,45 @@ Replace code below according to your needs.
 """
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, List, Sequence, Tuple, Union
+import numpy as np
+from aicsimageio.writers import OmeTiffWriter
+from qtpy.QtWidgets import QFileDialog
 
-if TYPE_CHECKING:
-    DataType = Union[Any, Sequence[Any]]
-    FullLayerData = Tuple[DataType, dict, str]
+def save_dialog(parent):
+    """
+    Opens a dialog to select a location to save a file
 
-
-def write_single_image(path: str, data: Any, meta: dict) -> List[str]:
-    """Writes a single image layer.
-    
     Parameters
     ----------
-    path : str
-        A string path indicating where to save the image file.
-    data : The layer data
-        The `.data` attribute from the napari layer.
-    meta : dict
-        A dictionary containing all other attributes from the napari layer
-        (excluding the `.data` layer attribute).
+    parent : QWidget
+        Parent widget for the dialog
 
     Returns
     -------
-    [path] : A list containing the string path to the saved file.
+    str
+        Path of selected file
     """
+    dialog = QFileDialog()
+    filepath, _ = dialog.getSaveFileName(
+        parent,
+        "Select location for TIFF-File to be created",
+        filter="TIFF files (*tif *.tiff)",
+    )
+    if not (filepath.endswith(".tiff") or filepath.endswith(".tif")):
+        filepath += ".tiff"
+    return filepath
 
-    # implement your writer logic here ...
 
-    # return path to any file(s) that were successfully written
-    return [path]
+def write_tiff(path: str, data: np.ndarray):
+    """
+    Write data to a TIFF file
 
-
-def write_multiple(path: str, data: List[FullLayerData]) -> List[str]:
-    """Writes multiple layers of different types.
-    
     Parameters
     ----------
     path : str
-        A string path indicating where to save the data file(s).
-    data : A list of layer tuples.
-        Tuples contain three elements: (data, meta, layer_type)
-        `data` is the layer data
-        `meta` is a dictionary containing all other metadata attributes
-        from the napari layer (excluding the `.data` layer attribute).
-        `layer_type` is a string, eg: "image", "labels", "surface", etc.
-
-    Returns
-    -------
-    [path] : A list containing (potentially multiple) string paths to the saved file(s).
+        Path to save the file
+    data : np.ndarray
+        Data to save
     """
-
-    # implement your writer logic here ...
-
-    # return path to any file(s) that were successfully written
-    return [path]
+    data = data.astype(np.uint16)
+    OmeTiffWriter.save(data, path, dim_order_out="YX")
